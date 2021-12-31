7cd1748a709f32b38ba575d9117c52f5c55f400c
JiYoon13 <44959513+JiYoon13@users.noreply.github.com>
2021-12-31 16:05:44 +0900

    Add files via upload

--- LibraryProject/src/main/java/com/library/borrow_list/BoListController.java
+++ LibraryProject/src/main/java/com/library/borrow_list/BoListController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.library.login.LoginDTO;
 
@@ -24,8 +25,8 @@ public class BoListController {
 	
 	// 대출현황 
 	@RequestMapping("/borrow_list")
-	public String Bolist(HttpServletRequest request, Model mo) {
-		
+	public ModelAndView Bolist(HttpServletRequest request, Model mo) {
+		ModelAndView mv = new ModelAndView();
 		//로그인 정보 가져와서 회원번호 저장
 		HttpSession session = request.getSession();
 		LoginDTO ldto = (LoginDTO)session.getAttribute("member");
@@ -34,9 +35,27 @@ public class BoListController {
 		// 회원번호로 대출현황 조회
 		BoListService boListService = sqlSession.getMapper(BoListService.class);
 		ArrayList<BoListDTO> bo_list = boListService.bo_list(mem_no);
-		mo.addAttribute("bo_list", bo_list);
 		
-		return "borrow_list";
+		String[] book_isbn = new String[11];
+		String[] book_title = new String[11];
+		String[] be_date = new String[11];
+		String[] be_return = new String[11];
+		String mem_id = bo_list.get(0).getMem_id();
+		int i = 0 ;
+		for(BoListDTO k : bo_list) {
+			book_isbn[i] = k.getBook_isbn();
+			book_title[i] = k.getBook_title();
+			be_date[i] = k.getBe_date();
+			be_return[i] = k.getBe_return();
+			i++;
+		}
+		mv.addObject("book_isbn", book_isbn);
+		mv.addObject("book_title", book_title);
+		mv.addObject("be_date", be_date);
+		mv.addObject("be_return", be_return);
+		mv.addObject("mem_id", mem_id);
+		mv.addObject("mem_no", mem_no);
+		return mv;
 	}
 }
 
